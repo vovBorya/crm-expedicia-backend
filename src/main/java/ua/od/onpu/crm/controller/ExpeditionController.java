768ac4b38be1653d329929787525a140fb56aa3a
Artyom Kazlanzhy <kazlanzhy@gmail.com>
2020-03-20 16:47:08 +0200

    fix

--- src/main/java/ua/od/onpu/crm/controller/ExpeditionController.java
+++ src/main/java/ua/od/onpu/crm/controller/ExpeditionController.java
@@ -3,6 +3,7 @@ package ua.od.onpu.crm.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import ua.od.onpu.crm.service.ExpeditionService;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @Slf4j
 @RequestMapping("/api/expeditions")
