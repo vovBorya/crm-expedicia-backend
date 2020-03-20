c3a501b213e5413ad4cab53bcfad3a220e39dced
Artyom Kazlanzhy <kazlanzhy@gmail.com>
2020-03-20 16:36:25 +0200

    minor fix

--- src/main/java/ua/od/onpu/crm/controller/CustomerController.java
+++ src/main/java/ua/od/onpu/crm/controller/CustomerController.java
@@ -3,6 +3,7 @@ package ua.od.onpu.crm.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import ua.od.onpu.crm.dto.CustomerDto;
 import ua.od.onpu.crm.service.CustomerService;
 
 import javax.servlet.http.HttpServletResponse;
-
 import java.util.List;
 
 @CrossOrigin
