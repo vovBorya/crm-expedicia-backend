77e3e2a8b599b1520f23591f32cc8edc5fa5c21a
vovBorya <vovbor02@gmail.com>
2020-05-29 15:45:05 +0300

    allowedHeaders

--- src/main/java/ua/od/onpu/crm/controller/JwtController.java
+++ src/main/java/ua/od/onpu/crm/controller/JwtController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class JwtController {
     @Autowired
     MyUserDetailsService userDetailsService;
 
+    @CrossOrigin(allowedHeaders = "Content-Type")
     @PostMapping(value = "/api/auth")
     public ResponseEntity<?> createAuthToken(@RequestBody AuthRequest request) throws BadCredentialsException {
         try {
